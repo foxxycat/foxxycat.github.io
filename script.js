f455a9431e18b812ac9362b6581ba71c95bfa3db
foxxycat <43220582+foxxycat@users.noreply.github.com>
2019-11-22 15:56:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $("#certButton").on("click", function(){
 		var card = $('<div class="col-sm-6"><div class="card" style="width: 18rem; text-align: center;"><img src="sololearn_jq.png" class="card-img-top" alt=""><div class="card-body"><h4 class="card-title">jQuery Tutorial course</h4><p class="card-text">2019</p><a href="https://www.sololearn.com/Certificate/1082-226804/pdf/" class="btn btn-primary" target="_blank">Посмотреть</a></div></div></div>');
 		$("eCert").append(card);
 		$("input").vall("");
+		alert("Done");
 	}
 	else{
 		alert("Поля пустые!");
