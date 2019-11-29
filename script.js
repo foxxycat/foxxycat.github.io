8e5e11461c284f411a8e2163c3ab7cac15bc76b5
foxxycat <43220582+foxxycat@users.noreply.github.com>
2019-11-29 07:08:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $("#certButton").on("click", function(){
 	let href = $("#hrefCert").val();
 	let year = $("#yearCert").val();
 	if (name, href, year != "") {
-		var card = $('<div class="col-sm-6"><div class="card" style="width: 18rem; text-align: center;"><img src="sololearn_jq.png" class="card-img-top" alt=""><div class="card-body"><h4 class="card-title">jQuery Tutorial course</h4><p class="card-text">2019</p><a href="https://www.sololearn.com/Certificate/1082-226804/pdf/" class="btn btn-primary" target="_blank">Посмотреть</a></div></div></div>');
+		var card = $('<div class="col-sm-6"><div class="card" style="width: 18rem; text-align: center;"><img src="sololearn_jq.png" class="card-img-top" alt=""><div class="card-body"><h4 class="card-title">jQuery Tutorial course</h4><p class="card-text">2019</p><a href="https://www.sololearn.com/Certificate/1082-226804/pdf/" class="btn btn-primary" target="_blank">Посмотреть</a></div></div></div>').html();
 		$("eCert").append(card);
 		$("input").vall("");
 		alert("Done");
